c9a947d4dc9edc889ba1fd3e8b93a10267409e89
Justin Osagie <osajustin@gmail.com>
2024-11-17 06:27:11 -0500

    Enabled speed and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import { ThemeModeScript } from "flowbite-react";
 import "./globals.css";
 
