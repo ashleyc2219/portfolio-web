be9a3d41d84679cd1054eb88cdf0158a6e9460db
Justin Osagie <osajustin@gmail.com>
2024-11-18 09:44:37 -0500

    enable email

--- components/sections/contactSection.tsx
+++ components/sections/contactSection.tsx
@@ -53,7 +53,7 @@ const ContactSection: React.FC = () => {
   return (
     <section
       id="contact"
-      className="scroll-mt-navbar-height pt-navbar-height bg-primary-white px-4 py-16 md:px-20 md:py-24"
+      className="scroll-mt-navbar-height bg-primary-white px-4 py-16 pt-navbar-height md:px-20 md:py-24"
     >
       <div className="mx-auto grid max-w-screen-lg grid-cols-1 gap-8 md:grid-cols-2">
         {/* Right Section: Text */}
@@ -110,8 +110,7 @@ const ContactSection: React.FC = () => {
             <button
               type="submit"
               className="w-full rounded-md bg-primary-black px-5 py-3 text-sm font-medium text-primary-white transition hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-primary-black md:w-auto"
-              disabled={true}
-              // disabled={isSubmitting}
+              disabled={isSubmitting}
             >
               {isSubmitting ? "Sending..." : "Get In Touch"}
             </button>
