c002a890d54260f6a1839ca92c9349e277ed7790
Justin Osagie <osajustin@gmail.com>
2024-12-18 12:31:43 -0500

    comments removed

--- config/heroConfig.ts
+++ config/heroConfig.ts
@@ -1,11 +1,11 @@
 export const heroConfig = {
   hero: {
-    name: "Firstname Lastname", // Replace with your name
-    primaryRole: "Frontend", // Replace with your primary role (e.g., Fullstack, Backend, etc.)
-    secondaryRole: "Developer", // Replace with your secondary role (e.g., UI/UX Designer, Software Engineer, etc.)
-    location: "Location", // Replace with your location (e.g., City, Country)
+    name: "Firstname Lastname",
+    primaryRole: "Frontend",
+    secondaryRole: "Developer",
+    location: "Location",
     description: `I’m [Your Name], a [Your Primary Role] passionate about crafting scalable web applications and robust backend systems. With expertise in [Technologies], I transform complex challenges into elegant solutions. Beyond coding, I enjoy [Hobbies/Interests]. Let’s create something extraordinary together!`,
-    resumeLink: "/resume/resume_Justin_Osagie.pdf", // Ensure this path is correct or update it
-    heroImage: "/heroSection/heroImageLarge.svg", // use public/heroSection/girl-heroImageLarge.svg for girl image
+    resumeLink: "/resume/resume_Justin_Osagie.pdf",
+    heroImage: "/heroSection/heroImageLarge.svg",
   },
 };
